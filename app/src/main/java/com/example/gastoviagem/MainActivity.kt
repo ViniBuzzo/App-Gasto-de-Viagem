f53d2b34ded70e36259a2810b5b02ddfea4784c5
ViniBuzzo <=>
2024-08-05 16:09:28 -0300

    Start commit

---
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.gastoviagem
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.gastoviagem.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.button_calculate){
+            calculate()
+        }
+    }
+
+    private fun isValid(): Boolean{
+        return (binding.editDistance.text.toString() != ""
+                && binding.editPrice.text.toString() != ""
+                && binding.editAutonomy.text.toString() != ""
+                && binding.editAutonomy.text.toString().toFloat() != 0f)
+    }
+
+
+    private fun calculate() {
+
+        if (isValid()) {
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+            val isRoundTrip = binding.switchRoundTrip.isChecked
+            val totalDistance = if (isRoundTrip) distance * 2 else distance
+            val totalValue = (totalDistance * price) / autonomy
+
+
+            binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        } else{
+            Toast.makeText(this, R.string.validation_fill_all_fields, Toast.LENGTH_SHORT).show()
+        }
+
+
+        //Toast.makeText(this, totalValueStr, Toast.LENGTH_SHORT).show()
+    }
+}
+
